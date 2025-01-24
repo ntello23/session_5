b194ae28ab5f1041bbeac0eb15a080bb755a3181
ntelloyoung <ntelloyoung@gmail.com>
2025-01-24 11:27:36 +0100

    Upload 'input_example.py'

---
+++ input_example.py
@@ -0,0 +1,11 @@
+
+name = input('What is yous name? ')
+
+# print('Hello ' + name)
+#
+# age = int(input('How old are you ' + name + '? '))
+#
+# print(name + ' based on my advanced calculations, you were born in ' + str(2024 - age))
+
+age2 = int(input(f'How old are you {name}? '))
+print(f'{name} were born in {2024-age2}')
