8f6ccfa638c674226b5c6fc698dd3f832d3af950
ntelloyoung <ntelloyoung@gmail.com>
2025-01-31 11:06:07 +0100

    Added for examples

---
+++ for_example.py
@@ -0,0 +1,14 @@
+
+#for i in range(1,10, 2):
+#    print(i)
+
+s = 0
+# for i in range(1, 11):
+#     s += i
+# print(s)
+i=0
+while i < 11:
+    s += i
+    i += 1
+
+print(s)
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@
 ## Expections
 ## If
 ## While
+## For
